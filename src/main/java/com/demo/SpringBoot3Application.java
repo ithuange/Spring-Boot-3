ec09fd16229afa45b3a271cc0deae4d34e4010e4
LHLLZSYLSC131456 <1156421151@qq.com>
2020-08-12 10:16:59 +0800

    测试数据六

--- src/main/java/com/demo/SpringBoot3Application.java
+++ src/main/java/com/demo/SpringBoot3Application.java
@@ -9,6 +9,6 @@ public class SpringBoot3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot3Application.class, args);
 		System.out.println("这是测试数据五");
+		System.out.println("我是测试数据六呀~~~~");
 	}
-
 }
