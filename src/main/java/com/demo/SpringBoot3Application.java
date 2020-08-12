6330768f22112172e1b905e28f6488e9325b6482
ithuange <63647686+ithuange@users.noreply.github.com>
2020-08-12 10:14:06 +0800

    Update SpringBoot3Application.java

--- src/main/java/com/demo/SpringBoot3Application.java
+++ src/main/java/com/demo/SpringBoot3Application.java
@@ -8,6 +8,7 @@ public class SpringBoot3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot3Application.class, args);
+		System.out.println("这是测试数据五");
 	}
 
 }
